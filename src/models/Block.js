d2f8a5544ce84cafe1750831021fc63bf7ca9057
zzc1112399 <132678433+zzc1112399@users.noreply.github.com>
2023-05-17 19:23:41 +0800

    lesson2

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,31 @@
 import sha256 from 'crypto-js/sha256.js'
 
-export const DIFFICULTY = 2
+export const DIFFICULTY = 3
 
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(MyChain,previousHash,height,hash) {
+    this.MyChain=MyChain
+    this.previousHash=previousHash
+    this.height=height
+    this.nonce=""
+    this.hash=hash
+  }
 
-  isValid() {}
+  isValid() {
+    let cnt=0;
+    for(let i of this.nonce){
+      if(i!="0")break;
+      cnt++;
+    }
+    return cnt>=DIFFICULTY
+  
+  }
 
-  setNonce(nonce) {}
+  setNonce(nonce) {
+    this.nonce=nonce
+  }
   
 }
 
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,17 +6,70 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name) {
+    this.name=name
+    this.genesis=null
+    this.blocks={}
+  }
 
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
   longestChain() {
+    let longestChain = []
+    let currentChain = []
 
+    // 从每个块开始，递归查找最长的区块链
+    for (const hash in this.blocks) {
+       var block = this.blocks[hash]
+      currentChain = []
 
-    return []
+      // 从当前块向前查找区块链
+      while (block) {
+        currentChain.unshift(block)
+        block = this.blocks[block.previousHash]
+      }
+
+      // 如果找到了更长的区块链，则更新longestChain
+      if (currentChain.length > longestChain.length) {
+        longestChain = currentChain
+      }
+    }
+
+    return longestChain
   }
 }
 
 export default Blockchain
+/*
+constructor(name) {
+    this.name = name
+    this.genesis = null
+    this.blocks = {}
+  }
+
+    longestChain() {
+      let longestChain = []
+      let currentChain = []
+  
+      // 从每个块开始，递归查找最长的区块链
+      for (const hash in this.blocks) {
+         var block = this.blocks[hash]
+        currentChain = []
+  
+        // 从当前块向前查找区块链
+        while (block) {
+          currentChain.unshift(block)
+          block = this.blocks[block.previousHash]
+        }
+  
+        // 如果找到了更长的区块链，则更新longestChain
+        if (currentChain.length > longestChain.length) {
+          longestChain = currentChain
+        }
+      }
+  
+      return longestChain
+    }
+*/
